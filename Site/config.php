455b0b668017542548d007cb67cadc691739f4d1
ViniAzz <vinicios_azambuja@estudante.sc.senai.br>
2020-04-14 15:38:07 -0300

    S.A

---
+++ Site/config.php
@@ -0,0 +1,7 @@
+<?php
+if ( !defined('BASEURL') )
+define('BASEURL', '/startbootstrap-agency-gh-pages/');
+if ( !defined('ABSPATH') )
+define('ABSPATH', dirname(__FILE__) . '/');
+define('HEADER_TEMPLATE', ABSPATH .'inc/header.php');
+?>
